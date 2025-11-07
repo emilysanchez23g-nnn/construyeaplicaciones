1fae253911beaf181eee6195f588ddb119769ad8
emilysanchez23g-nnn <emilysanchez23g@gmail.com>
2025-11-07 07:46:53 -0800

    comit inicial

---
+++ store/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_lenght=225)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+        def _str_(self):
+            return self.name
+        
+class Item (models.Model):
+    category = models.ForeignKey(Category, related_name='Items', on_delete=models.CASCADE)
+    name = models.CharField(max_lenght=225)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name ='items', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+def _str_(self):
+    return self.name
+
